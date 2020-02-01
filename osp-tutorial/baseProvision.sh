d938e8e8ca9b376156580eddc4fc795466c6c40c
Flavio Fernandes <flavio@flaviof.com>
2020-02-01 12:49:19 -0500

    Make it devstack friendly
    See:
    https://github.com/ovn-org/ovn/blob/master/Documentation/tutorials/ovn-openstack.rst

--- osp-tutorial/baseProvision.sh
+++ osp-tutorial/baseProvision.sh
@@ -7,5 +7,6 @@ set -o errexit
 # commented out the line below, because qemu in virtual box will never make it
 ## [ -e /dev/kvm ] || { echo "PROBLEM, you need to ensure nesting is enabled"; exit 1; }
 
-yum install -y git emacs wget
+yum install -y git emacs vim wget tmux
+yum install -y python3 python3-devel
 
