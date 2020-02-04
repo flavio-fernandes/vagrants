4fed5fa0ea66a3f2e3be644927c8a0bb1d6d6dbb
Flavio Fernandes <flaviof@redhat.com>
2020-02-04 05:42:16 -0500

    c7 master

--- osp-tutorial/baseProvision.sh
+++ osp-tutorial/baseProvision.sh
@@ -5,10 +5,13 @@
 set -o errexit
 
 # commented out the line below, because qemu in virtual box will never make it
-## [ -e /dev/kvm ] || { echo "PROBLEM, you need to ensure nesting is enabled"; exit 1; }
+[ -e /dev/kvm ] || { echo "PROBLEM, you need to ensure hv can nest"; exit 1; }
+grep -q Y /sys/module/kvm_intel/parameters/nested || {
+  sudo rmmod kvm-intel
+  sudo sh -c "echo 'options kvm-intel nested=y' >> /etc/modprobe.d/dist.conf"
+  sudo modprobe kvm-intel
+}
+modinfo kvm_intel | grep -q 'nested:bool' || { echo "PROBLEM, nesting did not enable"; exit 1; }
 
-#yum install -y git emacs wget
-apt-get update
-apt install -y git emacs wget
-apt install -y vim telnet tmate
-apt install -y python3-dev
+yum install -y git emacs wget vim telnet
+yum install -y python3 python3-devel
